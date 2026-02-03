5a32e06b03e61901c0b3f4dca9a2c463a636f6cf
TallothEndill <1220890+tallothendill@users.noreply.github.com>
2026-02-03 14:48:28 +0100

    Update ServoZero_Arduino.py

--- MedusaHC/ServoZero_Arduino.py
+++ MedusaHC/ServoZero_Arduino.py
@@ -8,18 +8,34 @@
 
 int pos;  // position of servo (range between 0 and 180; 90 is the neutral position) 
 int servoPin=9; // analog 9 pin for servo
-int servoDelay = 5000;  // Delay servo by 5 seconds 
+int servoDelay = 2000;  // Delay servo by 2 seconds 
 
 Servo myTestSubject;  // Declare servo object
 
 void setup() {
 Serial.begin(9600);
 myTestSubject.attach(servoPin);
+    pos=0;  //  Move servo to position 0 = minimum position
+    myTestSubject.write(pos);
+    Serial.println(pos);  // Display position 
+    delay(servoDelay);
+
+    pos=90;  //  Move servo to position 90 = middle position
+    myTestSubject.write(pos);
+    Serial.println(pos);  // Display position 
+    delay(servoDelay);
+
+    pos=180;  //  Move servo to position 180 = maximum position
+    myTestSubject.write(pos);
+    Serial.println(pos);  // Display position 
+    delay(servoDelay);
+
+    pos=90;  //  Move servo to position 90 = the neutral position
+    myTestSubject.write(pos);
+    Serial.println(pos);  // Display position 
+    delay(servoDelay);
 }
 
 void loop() {
-pos=90;  //  Move servo to position 90 = the neutral position
-myTestSubject.write(pos);
-Serial.println(pos);  // Display position 
-delay(servoDelay);
+
 }
